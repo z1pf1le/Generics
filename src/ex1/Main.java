3941c9b7766dde55a298dde6bba33832cf4404e8
z1pf1le <total.zip.file@gmail.com>
2023-11-24 12:57:24 +0300

    selfedu

--- src/ex1/Main.java
+++ src/ex1/Main.java
@@ -1,18 +1,22 @@
 package ex1;
 
+import alishev.Dog;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Zoo<Integer, String> zoo = new Zoo<>();
+        List<Dog> dogList = new ArrayList<>();
+        List<Integer> intlist = new ArrayList<>();
         zoo.x = 1;
         zoo.y = "tygy";
         zoo.print();
-
+        test(dogList );
+        test(intlist);
     }
 
     public static void test(List<?> list) {
-
     }
 }
---
+++ src/selfedu/Main.java
@@ -0,0 +1,35 @@
+package selfedu;
+
+import java.awt.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Point<Integer> pt = new Point<>(12, 22);
+        Point<Double> ptD = new Point<>(12.5, 22.6);
+
+
+        System.out.println(pt.x + " " + pt.y);
+        System.out.println(ptD.x + " " + ptD.y);
+
+        for(Object coord: ptD.getCoords())
+            System.out.println((double)coord);
+
+    }
+}
+
+class Point<T> {
+    public T x, y;
+
+    public Point(T x, T y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Object[] getCoords() {
+        return new Object[] {x, y};
+    }
+
+
+}
+
+
